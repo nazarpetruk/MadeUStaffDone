60cd79ba1de583135c167b2065dfe2e7a5d9d69f
nazar petruk <nazarpetruk15@gmail.com>
2019-08-05 20:06:52 +0200

    coreData involved create new item and save data from alert in.

--- MadeUStaffDone/Controllers/ToDoListViewController.swift
+++ MadeUStaffDone/Controllers/ToDoListViewController.swift
@@ -7,15 +7,17 @@
 //
 
 import UIKit
+import CoreData
 
 class ToDoListViewController: UITableViewController {
     
-    var itemArray = ["Few Stuff to Do", "To finish Project", "Watch Tut."]
+    var itemArray = [Item]()
+    let context = (UIApplication.shared.delegate as! AppDelegate).persistentContainer.viewContext
     
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+
     }
     //MARK: Table view data source methods
     override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
@@ -24,32 +26,35 @@ class ToDoListViewController: UITableViewController {
     
     override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
         let cell = tableView.dequeueReusableCell(withIdentifier: "TodoItemCell", for: indexPath)
-        cell.textLabel?.text = itemArray[indexPath.row]
+        
+        let item = itemArray[indexPath.row]
+        
+        cell.textLabel?.text = item.title
+        
+        cell.accessoryType = item.done ? .checkmark : .none
+        
         return cell
     }
     
     //MARK: TableView delegate Methods
     override func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
-        
-        if tableView.cellForRow(at: indexPath)?.accessoryType == .checkmark{
-            tableView.cellForRow(at: indexPath)?.accessoryType = .none
-        }else{
-            tableView.cellForRow(at: indexPath)?.accessoryType = .checkmark
-        }
-        
+        itemArray[indexPath.row].done = !itemArray[indexPath.row].done
+        tableView.reloadData()
         tableView.deselectRow(at: indexPath, animated: true)
         
     }
     //Mark: Add new element func. using alert and text field
-    
     
     @IBAction func newItemBtnPressed(_ sender: UIBarButtonItem) {
         var textField = UITextField()
         let alert = UIAlertController(title: "Add new Staff ToDo", message: "", preferredStyle: .alert)
         let action = UIAlertAction(title: "Add Item", style:.default){(action) in
             
-            self.itemArray.append(textField.text!)
-            self.tableView.reloadData()
+            let newItem = Item(context: self.context)
+            newItem.title = textField.text!
+            newItem.done = false
+            self.itemArray.append(newItem)
+            self.saveItem()
         }
         alert.addTextField { (alertTextField) in
             alertTextField.placeholder = "Name new Item"
@@ -59,6 +64,16 @@ class ToDoListViewController: UITableViewController {
         present(alert,animated: true,completion: nil)
     }
     
+    
+    
+    func saveItem(){
+        do{
+           try context.save()
+        }catch{
+            print("Problem with saving \(error)")
+        }
+        self.tableView.reloadData()
+    }
     
     
 }
